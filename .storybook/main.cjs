d067cb72ead2c0b2843e0544cd243ef2a6f94e0a
Morgana <morganabagatini@gmail.com>
2022-10-13 21:23:43 -0300

    Storybook config with vite

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,7 +15,7 @@ module.exports = {
 	"features": {
 		"storyStoreV7": true
 	},
-	viteConfig: (config, { configType }) => {
+	viteFinal: (config, { configType }) => {
 		if (configType === 'PRODUCTION') {
 			config.base = '/ignite-lab-design-system/'
 		}
